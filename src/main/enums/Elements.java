f69145c282a01fad50f63b85d4501624ec3177f8
Aidai Suiumbaeva <aidisuiumbaeva@gmail.com>
2022-09-01 17:08:55 +0600

    add Cu

--- src/main/enums/Elements.java
+++ src/main/enums/Elements.java
@@ -2,8 +2,9 @@ package main.enums;
 
 public enum Elements {
     Au("Gold", 2),
-    O("O", 5),
-    Cu("Медь", 3);
+    O("Oxygenium", 5),
+    Ra("Radium", 5),
+    Cu("Cuprum", 3);
 
     private String name;
     private int num;
